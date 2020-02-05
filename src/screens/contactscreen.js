b76815796b9fdd4f8694eb08c808bd2aec88279e
Yami <daoanhduc209@gmail.com>
2020-02-05 10:43:42 +0700

    Implement I18n with hook

--- src/screens/contactscreen.js
+++ src/screens/contactscreen.js
@@ -1,13 +1,31 @@
-import React, { PureComponent } from 'react';
+import React,{useEffect, useState}  from 'react';
 import {
-    View, Text
+    View, Text, TouchableOpacity
 } from 'react-native';
-import AppText from '../components/app-text';
+import I18n from 'react-native-i18n';
 
 const ContactScreen = props => {
+        const [langValue, setLangValue] = useState('');
+        function onChangeTextSearch(value) {
+            setLangValue(value);
+            I18n.locale = value;
+        }
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <AppText i18nKey={'this-is-contact-page'}>This is contact screen</AppText>
+                <Text>{I18n.t('this-is-contact-page')}</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text>{I18n.t('set-language')}</Text>
+                    <TouchableOpacity
+                        onPress={() => onChangeTextSearch('vi')}
+                                      style={{ marginLeft: 20 }}>
+                        <Text style={{ color: langValue === 'vi' ? 'blue' : 'grey' }}>Việt Nam</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => onChangeTextSearch('en')}
+                                      style={{ marginLeft: 5 }}>
+                        <Text style={{ color: langValue === 'en' ? 'blue' : 'grey' }}>English</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     };
